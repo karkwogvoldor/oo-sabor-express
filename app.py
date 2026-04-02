eb25357de1f6b16d37ad1bb310ed7fc7639f44ec
Jerônimo <karkwogvoldor@gmail.com>
2026-04-02 14:35:04 -0300

    implementando avaliacoes

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_jorje_sushi = Restaurante('Jorje Sushibar', 'Japonesa')
 restaurante_pimenta_loka = Restaurante('Pimenta Loka', 'Mexicana')
 restaurante_pimenta_loka.alternar_estado()
+restaurante_praca.receber_avaliacao('Jeronimo', 5)
+restaurante_praca.receber_avaliacao('Maria', 4)
+restaurante_praca.receber_avaliacao('João', 3)
 
 def main():
     Restaurante.listar_restaurantes()
